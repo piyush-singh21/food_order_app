fbdf55ac285343026ad822a58d22191880e7512d
Piyush Singh <1806397@kiit.ac.in>
2022-12-09 18:32:33 +0530

    4thA commit:another minor change in id

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -10,7 +10,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm id={props.key} />
       </div>
     </li>
   );
